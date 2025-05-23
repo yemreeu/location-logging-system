897bbe34172fa87959b46cce9783a8bf96c4d384
Yunus <y.emreuyanik@gmail.com>
2025-05-23 14:04:53 +0300

    fix: enable transformation in global validation pipe

--- area-service/src/main.ts
+++ area-service/src/main.ts
@@ -7,7 +7,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
   
   app.connectMicroservice<MicroserviceOptions>({
